9186bf2ce0d9b3387a030128ac48fae4815a7313
pierre <pierre@pvway.com>
2025-07-30 18:29:51 +0200

    EnumConv (ongoing) LoggerService (ongoing)

---
+++ src/EnumConv/nc09/Components/pvNugsEnumConvNc9/EnumConvert.cs
@@ -0,0 +1,92 @@
+﻿using System.ComponentModel;
+
+namespace pvNugsEnumConvNc9;
+
+public static class EnumConvert
+{
+    public static string GetCode(this Enum value)
+    {
+        var rawDescription = GetRawDescription(value);
+        if (!rawDescription.Contains(',')) return rawDescription;
+        var codeList = rawDescription.Split(',');
+        return codeList[0];
+    }
+    
+    private static string GetRawDescription(this Enum value)
+    {
+        var fieldInfo = value
+            .GetType()
+            .GetField(value.ToString())!;
+        DescriptionAttribute[]? descriptionAttributes = null;
+        try
+        {
+            descriptionAttributes = (DescriptionAttribute[]?)fieldInfo
+                .GetCustomAttributes(
+                    typeof(DescriptionAttribute), false);
+        }
+        catch (Exception e)
+        {
+            // nop
+        }
+        if (descriptionAttributes is not null 
+            && descriptionAttributes.Length > 0)
+            return descriptionAttributes[0].Description;
+        
+        throw new ArgumentOutOfRangeException(
+            nameof(value), value, null);
+    }
+
+    public static T GetValue<T>(
+        string code,
+        Func<string, string, bool> match) where T : Enum
+    {
+        if (string.IsNullOrEmpty(code)) 
+            throw new ArgumentNullException(nameof(code));
+        var found = TryFindCode<T>(code, match, out var value);
+        if (found) return value!;
+        throw new ArgumentOutOfRangeException(
+                nameof(code), code, null);
+    }
+    
+    public static T GetValue<T>(
+        string? code,
+        T defaultValue) where T : Enum =>
+        GetValue(code, defaultValue, Matcher);
+
+    public static T GetValue<T>(
+        string? code,
+        T defaultValue,
+        Func<string, string, bool> match) where T : Enum
+    {
+        if (string.IsNullOrEmpty(code)) return defaultValue;
+        
+        var found = TryFindCode<T>(code, match, out var value);
+        
+        return found ? value! : defaultValue;
+    }
+
+    private static bool TryFindCode<T>(
+        string code,
+        Func<string, string, bool> match,
+        out T? value) where T : Enum
+    {
+        value = default;
+        var values = 
+            Enum.GetValues(typeof(T)).Cast<T>();
+        foreach (var enumValue in values)
+        {
+            var rawDescription = enumValue.GetRawDescription();
+            var vCodes = rawDescription.Split(',',
+                    StringSplitOptions.TrimEntries)
+                .ToList();
+            if (!vCodes.Exists(vCode => match(code, vCode))) continue;
+            value = enumValue;
+            return true;
+        }
+        return false;
+    }
+    
+    private static bool Matcher(string x, string y) =>
+        x.Equals(y, StringComparison.InvariantCultureIgnoreCase);
+    
+}
---
+++ src/Logger/nc09/Components/pvNugsLoggerNc9/PvNugsLoggerConfig.cs
@@ -0,0 +1,31 @@
+﻿using pvNugsEnumConvNc9;
+using pvNugsLoggerNc9Abstractions;
+
+namespace pvNugsLoggerNc9;
+
+public class PvNugsLoggerConfig
+{
+    public const string Section = nameof(PvNugsLoggerConfig);
+
+    public string MinLogLevel { get; set; } = null!;
+
+    public SeverityEnu MinLevel => MinLogLevel.ToLowerInvariant() switch
+    {
+        "trace" or "t" or "verbose" or "verb" or "vrb" or "v" => SeverityEnu.Trace,
+        "debug" or "dbg" or "d" => SeverityEnu.Debug,
+        "inf" or "info" or "information" or "i" => SeverityEnu.Info,
+        "warning" or "warn" or "w" => SeverityEnu.Warning,
+        "error" or "err" or "e" => SeverityEnu.Error,
+        "fatal" or "f" or "critic" or "critical" or "c" => SeverityEnu.Fatal,
+        _ => SeverityEnu.Trace
+    };
+
+    public PvNugsLoggerConfig()
+    {
+    }
+
+    public PvNugsLoggerConfig(SeverityEnu minLogLevel)
+    {
+        MinLogLevel = minLogLevel.GetCode();
+    }
+}
