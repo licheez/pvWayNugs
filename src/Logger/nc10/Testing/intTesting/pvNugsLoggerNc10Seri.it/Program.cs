53ac8a3dee6d7668cbe718cb8898ef8c26771c95
pierre <pierrevanwall@gmail.com>
2026-06-11 10:54:42 +0200

    Add integration project for pvNugsLoggerNc10Seri with console logging support

---
+++ src/Logger/nc10/Testing/intTesting/pvNugsLoggerNc10Seri.it/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using pvNugsLoggerNc6Abstractions;
+using pvNugsLoggerNc6Seri;
+
+Console.WriteLine("Integration console for pvNugsLoggerNc9Hybrid");
+
+var inMemSettings = new Dictionary<string, string>
+{
+    // SERILOG
+    { "PvNugsLoggerConfig:MinLogLevel", "trace" },
+};
+
+var config = new ConfigurationBuilder()
+    .AddInMemoryCollection(inMemSettings!)
+    .Build();
+
+var services = new ServiceCollection();
+
+services.TryAddPvNugsLoggerSeriService(config);
+
+var sp = services.BuildServiceProvider();
+var cLogger = sp.GetRequiredService<IConsoleLoggerService>();
+
+await cLogger.LogAsync("Logging to the console with Serilog ", SeverityEnu.Trace);
+await cLogger.LogAsync("Logging to the console with Serilog " /*, SeverityEnu.Debug*/);
+await cLogger.LogAsync("Logging to the console with Serilog ", SeverityEnu.Info);
+await cLogger.LogAsync("Logging to the console with Serilog ", SeverityEnu.Warning);
+await cLogger.LogAsync("Logging to the console with Serilog ", SeverityEnu.Error);
+await cLogger.LogAsync("Logging to the console with Serilog ", SeverityEnu.Fatal);
